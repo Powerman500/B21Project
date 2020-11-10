6aad3765b6e62ca49aa7b3d7e6d632bc9112e2cb
Powerman500 <73196491+Powerman500@users.noreply.github.com>
2020-11-10 12:27:13 -0500

    all happy at last

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
         System.out.println("Hello");
         // B21 is good at IntelliJ
-        // Mac and Windows user are happy to see commit
+        // Mac and Windows user are happy to see commit dialog finally
 
 
     }

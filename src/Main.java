9b33c3cbfa53c1cb30adf0120612c38696c9ba6b
Powerman500 <73196491+Powerman500@users.noreply.github.com>
2020-11-10 12:49:07 -0500

    wow ,here we go

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,8 @@ public class Main {
         System.out.println("Hello GitHub, Hello World");
         // B21 is good at IntelliJ
         // Mac and Windows user are happy to see commit dialog finally
-
+        // published
+        // it's time to push !!!
 
     }
 }

38225a160510997117cce60aa0e3b7b7d86b8625
Powerman500 <73196491+Powerman500@users.noreply.github.com>
2020-11-10 12:36:33 -0500

    perfection the code

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello");
+        System.out.println("Hello GitHub, Hello World");
         // B21 is good at IntelliJ
         // Mac and Windows user are happy to see commit dialog finally
 
